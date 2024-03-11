0ea7c467138c37be24ad98c3d1cceb8b9f385e4b
Amine OUADRANI <ouadrani@neoledge.com>
2024-03-11 23:48:33 +0100

    feat: easy-non-constructible-change problem solution

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Time complexity O(NlogN) Space complexity O(1)
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] coins = { 5, 7, 1, 1, 2, 3, 22 };
+        int result = NonConstructibleChange(coins);
+        Console.WriteLine("Result: " + result);
+    }
+
+    public static int NonConstructibleChange(int[] coins)
+    {
+        if (coins.Length < 1) return 1;
+        Array.Sort(coins); //O(NlogN)
+
+        int change = 0;
+        foreach (int elt in coins)
+        { //O(N)
+            if (elt > change + 1)
+                return change + 1;
+            change = elt + change;
+        }
+
+        return change + 1;
+    }
+}
